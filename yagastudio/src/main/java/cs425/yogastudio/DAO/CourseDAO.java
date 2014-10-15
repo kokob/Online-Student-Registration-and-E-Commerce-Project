47cc04bf6f69c7f4bb0ab65d1fb9e73c18f20911
b <b@10.10.14.104>
2014-10-15 11:11:45 -0500

    added courseDAO

---
+++ yagastudio/src/main/java/cs425/yogastudio/DAO/CourseDAO.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs425.yogastudio.DAO;
+
+import cs425.yogastudio.entity.Course;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author b
+ */
+@Transactional(propagation=Propagation.MANDATORY)
+public class CourseDAO {
+
+    private SessionFactory sessionFactory;
+
+    public CourseDAO() {
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    public void addCourse(Course course) {
+
+        sessionFactory.getCurrentSession().persist(course);
+
+    }
+
+    public List<Course> getAll() {
+        List<Course> courses = sessionFactory.getCurrentSession().createQuery("from course").list();
+
+        return courses;
+    }
+
+    public Course get(int id) {
+        return (Course) sessionFactory.getCurrentSession().get(Course.class, id);
+    }
+
+    public void update(int courseId, Course course) {
+        sessionFactory.getCurrentSession().update(course);
+    }
+
+    public void delete(int courseId) {
+        sessionFactory.getCurrentSession().delete(courseId);
+    }
+
+}
